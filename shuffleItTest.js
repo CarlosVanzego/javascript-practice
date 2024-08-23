63b7869b227dac68c4ddc10915102d22215119f8
Your Name <carlosvanzego96@gmail.com>
2024-08-23 18:18:12 -0400

    edits

--- shuffleItTest.js
+++ shuffleItTest.js
@@ -1,10 +1,10 @@
-function shuffleIt(arr,...p){
+function shuffleIt(arr,...c){
   // Iterate over each pair of indices in the variable-length argument p
-    for(i = 0; i < p.length; i++){
+    for(i = 0; i < c.length; i++){
   // Get the first index of the current pair   
-      let a = p[i][0];
+      let a = c[i][0];
   // Get the second index of the current pair  
-      let b = p[i][1];
+      let b = c[i][1];
   // Store the value at index a in a temporary variable  
       let temp = arr[a];
   // Assign the value stored in index b to index a    
@@ -16,5 +16,5 @@ function shuffleIt(arr,...p){
     return arr
   }
   
-  console.log(shuffleIt([1, 2, 3, 4], [0, 3], [1, 2]));
-  console.log(shuffleIt([10, 20, 30, 40], [0, 2], [1, 3]));
+  console.log(shuffleIt([1, 2, 3, 4], [0, 3], [1, 2])); //
+  console.log(shuffleIt([10, 20, 30, 40], [0, 2], [1, 3])); //
